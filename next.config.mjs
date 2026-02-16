ae5d19fdf8e934755f7df3d87cb27093266b1dec
hristofiziev-netizen <hristofiziev@gmail.com>
2026-02-15 16:13:17 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(process.cwd());
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(process.cwd()),
+    };
     return config;
   },
 };
